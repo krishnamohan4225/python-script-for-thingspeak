f6ba697810b8f177ac22e55abff49acb0cd81d06
krishnamohan4225 <krishna.pvkm@gmail.com>
2018-08-10 13:46:24 +0530

    Update thingspeak_python.py
    showing values of only field1 that is 1 or 0. 
    1 for intrusion detected and 0 for intrusion not detected.

--- thingspeak_python.py
+++ thingspeak_python.py
@@ -3,3 +3,17 @@ response = req.urlopen('https://api.thingspeak.com/channels/556464/feeds.json?re
 print (response.read())
 
 
+import urllib.request
+import json
+import time
+from pprint import pprint
+
+while True:
+  TS = urllib.request.urlopen("https://api.thingspeak.com/channels/556464/feeds.json?results=1")
+
+  response = TS.read()
+  data=json.loads(response)
+  b= data['feeds'][0]['field1']
+  print (b)
+  time.sleep(5)
+  TS.close()
