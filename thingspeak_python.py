5edddc89be072982ea2281d22b9d69ab4d903277
krishnamohan4225 <krishna.pvkm@gmail.com>
2018-09-26 13:28:44 +0530

    Update thingspeak_python.py

--- thingspeak_python.py
+++ thingspeak_python.py
@@ -1,8 +1,3 @@
-import urllib.request as req
-response = req.urlopen('https://api.thingspeak.com/channels/556464/feeds.json?results=1')
-print (response.read())
-
-
 import urllib.request
 import json
 import time
